88ac90c06d68bc6afb5ce5fb9a26707357fbc00d
Bobby <bm74@njit.edu>
2024-10-18 19:38:29 -0400

    Anyone who uses should update param

--- mysqlconnect.php
+++ mysqlconnect.php
@@ -1,7 +1,7 @@
 #!/usr/bin/php
 <?php
 
-$mydb = new mysqli('127.0.0.1','testUser','12345','testdb');
+$mydb = new mysqli('10.241.148.28','bobby','mashp','loginTest');
 
 if ($mydb->errno != 0)
 {
@@ -11,7 +11,7 @@ if ($mydb->errno != 0)
 
 echo "successfully connected to database".PHP_EOL;
 
-$query = "select * from students;";
+$query = "select * from users;";
 
 $response = $mydb->query($query);
 if ($mydb->errno != 0)
